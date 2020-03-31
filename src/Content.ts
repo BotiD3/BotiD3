ca96771f6d5608901427dcd77a4114ad73617fe5
BotiD3 <nagybotond123@gmail.com>
2020-03-31 17:11:17 +0200

    final

--- src/Content.ts
+++ src/Content.ts
@@ -1,4 +1,4 @@
-﻿import fs, { createWriteStream } from "fs";
+﻿import fs from "fs";
 import http from "http";
 import url from "url";
 import Megoldas from "./megoldas";
@@ -30,12 +30,12 @@ export default class Content {
 
         // Kezd a kódolást innen -->
         const megoldas = new Megoldas("jarmu.txt");
-        res.write(`2.feladat: ${megoldas.MunkaidoOraban()} órát dolgoztak\n`);
+        res.write(`2.feladat: ${megoldas.MunkaidoOraban()} órát dolgoztak a rendőrök\n`);
 
-        res.write("3.feladat:\n");
+        res.write("3.feladat: Műszakilag ellenőrzött járművek:\n");
         megoldas.EllenorzottAutok().forEach((i) => res.write("\t" + i + "\n"));
 
-        res.write("4.feladat:\n");
+        res.write("4.feladat: Elhaladó járművek kategóriánként:\n");
         res.write(`\tSzemélyautó: ${megoldas.Kategoriak()[0]}\n`);
         res.write(`\tAutóbusz: ${megoldas.Kategoriak()[1]}\n`);
         res.write(`\tKamion: ${megoldas.Kategoriak()[2]}\n`);
@@ -50,6 +50,25 @@ export default class Content {
         megoldas.KeresettRendszam(rendszamInput).forEach((rendszam) => res.write(`\t${rendszam}\n`));
 
         megoldas.AllomanybaIr("vizsgalt.txt", megoldas.VizsgaltAutok());
+
+        res.write("\n\n<u>GitHub repository:</u> ");
+        res.write("<a href='https://github.com/BotiD3/Erettsegi_KozutiEllenorzes/' target='_blank'>GitHub</a><br>");
+
+        res.write("\n\n<u>A vizsgalt.txt fájl:</u>\n");
+        try {
+            fs.readFileSync("vizsgalt.txt")
+                .toString()
+                .split("\n")
+                .forEach((i) => res.write(`${i.trim()}<br>`));
+        } catch (error) {
+            res.write("Hiba: " + (error as Error).message + "\n");
+        }
+
+        res.write("\n\n<u>A forrás jarmu.txt fájl:</u>\n");
+        fs.readFileSync("jarmu.txt")
+            .toString()
+            .split("\n")
+            .forEach((i) => res.write(`${i.trim()}<br>`));
 
         // <---- Fejezd be a kódolást
 
